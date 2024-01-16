c85556b053aaa225613f591738f7d0077407abe4
denyschr <denprogman99@gmail.com>
2024-01-16 12:03:18 +0200

    add: cart page

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
         product: "./product.html",
         category: "./category.html",
         cart: "./cart.html",
+        database: "./database",
       },
     },
   },
